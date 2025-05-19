660afbb72808736c4a5a7764540310bd53ae1f69
Duke <duke@fungies.io>
2025-05-19 17:55:45 +0200

    Add user-based tables migration

---
+++ auth.ts
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./db/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compareSync } from "bcrypt";
+import type { NextAuthConfig } from "next-auth";
+
+export const config = {
+  pages: {
+    signIn: "/sign-in",
+    signOut: "/sign-out",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) {
+          return null;
+        }
+        const user = await prisma.user.findFirst({
+          where: { email: credentials.email as string },
+        });
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
+          if (isMatch) {
+            return {
+              id: user.id,
+              email: user.email,
+              name: user.name,
+              role: user.role,
+            };
+          }
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      session.user.id = token.sub;
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -24,3 +24,8 @@ export const insertProductSchema = z.object({
 });
 
 export const updateProductSchema = insertProductSchema.partial();
+
+export const signInSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
